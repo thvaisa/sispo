45347723ef45dca72908f09302fe292ec8b22ad6
YgabrielsY <33376611+YgabrielsY@users.noreply.github.com>
2020-03-07 23:52:57 +0200

    Use install_deps

--- build/blender/temp_install.sh
+++ build/blender/temp_install.sh
@@ -15,22 +15,26 @@ git submodule foreach git checkout master
 git submodule foreach pull --rebase origin master
 
 # Dependencies
-#chmod +x build_files/build_environment/install_deps.sh
-#./build_files/build_environment/install_deps.sh --with-all --no-confirm
+chmod +x build_files/build_environment/install_deps.sh
+./build_files/build_environment/install_deps.sh --with-all --no-confirm
 
 # Update files
 make update
 
+# Bpy
+make bpy
+make install
+
 # Configure and install blender bpy
-cd ..
-[[ -d build_blender_bpy ]] || mkdir build_blender_bpy
-cd build_blender_bpy
-cmake \
-	-C ../blender/build_files/cmake/config/bpy_module.cmake \
-	-S ../blender \
-    -DWITH_IMAGE_OPENEXR=ON \
-    -DPYTHON_SITE_PACKAGES=/home/travis/miniconda/envs/sispo/lib/python3.7/site-packages \
-    -DPYTHON_LIBRARY=/home/travis/miniconda/envs/sispo/lib/python3.7/config-3.7m-x86_64-linux-gnu/libpython3.7m.so \
-    -DPYTHON_LIBPATH=/home/travis/miniconda/envs/sispo/lib \
-    -DPYTHON_INCLUDE_DIR=/home/travis/miniconda/envs/sispo/include \
-    -DPYTHON_INCLUDE_CONFIG_DIR=/home/travis/miniconda/envs/sispo/lib/python3.7/config-3.7m-x86_64-linux-gnu
+#cd ..
+#[[ -d build_blender_bpy ]] || mkdir build_blender_bpy
+#cd build_blender_bpy
+#cmake \
+#	-C ../blender/build_files/cmake/config/bpy_module.cmake \
+#	-S ../blender \
+#    -DWITH_IMAGE_OPENEXR=ON \
+#    -DPYTHON_SITE_PACKAGES:FILEPATH=/home/travis/miniconda/envs/sispo/lib/python3.7/site-packages \
+#    -DPYTHON_LIBRARY:FILEPATH=/home/travis/miniconda/envs/sispo/lib/python3.7/config-3.7m-x86_64-linux-gnu/libpython3.7m.so \
+#    -DPYTHON_LIBPATH:PATH=/home/travis/miniconda/envs/sispo/lib \
+#    -DPYTHON_INCLUDE_DIR:PATH=/home/travis/miniconda/envs/sispo/include/python3.7m \
+#    -DPYTHON_INCLUDE_CONFIG_DIR:PATH=/home/travis/miniconda/envs/sispo/include/python3.7m 
