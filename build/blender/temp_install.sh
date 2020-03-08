1e23e3b2817d48eb307d3a4934746d33375a27e7
YgabrielsY <33376611+YgabrielsY@users.noreply.github.com>
2020-03-08 19:42:43 +0200

    Remove if again

--- build/blender/temp_install.sh
+++ build/blender/temp_install.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
-if [[ -f /home/travis/build/YgabrielsY/sispo/software/blender/build_linux_bpy/bin/bpy.so ]]; then
-    echo "Using cached bpy"
-    cd software/blender/build_linux_bpy
-    make install
-else
-    echo "Installing blender as a python module start"
+#if [[ -f /home/travis/build/YgabrielsY/sispo/software/blender/build_linux_bpy/bin/bpy.so ]]; then
+#    echo "Using cached bpy"
+#    cd software/blender/build_linux_bpy
+#    make install
+#else
+echo "Installing blender as a python module start"
 
-    # Creating blender directory
-    cd software
-    [[ -d blender ]] || mkdir blender  
-    cd blender
+# Creating blender directory
+cd software
+[[ -d blender ]] || mkdir blender  
+cd blender
 
-    # Get blender repo
-    git clone https://git.blender.org/blender.git
-    cd blender
-    #git submodule update --init --recursive
-    #git submodule foreach git checkout master
-    #git submodule foreach pull --rebase origin master
+# Get blender repo
+git clone https://git.blender.org/blender.git
+cd blender
+#git submodule update --init --recursive
+#git submodule foreach git checkout master
+#git submodule foreach pull --rebase origin master
 
-    # Dependencies
-    chmod +x build_files/build_environment/install_deps.sh
-    ./build_files/build_environment/install_deps.sh --with-all --no-confirm --skip-oidn --skip-osd
+# Dependencies
+chmod +x build_files/build_environment/install_deps.sh
+./build_files/build_environment/install_deps.sh --with-all --no-confirm --skip-oidn --skip-osd
 
-    # Update files
-    make update
+# Update files
+make update
 
-    # Bpy
-    BUILD_CMAKE_ARGS="-DPYTHON_SITE_PACKAGES:FILEPATH=/home/travis/miniconda/envs/sispo/lib/python3.7/site-packages"
-    export BUILD_CMAKE_ARGS
-    make bpy
-fi
+# Bpy
+BUILD_CMAKE_ARGS="-DPYTHON_SITE_PACKAGES:FILEPATH=/home/travis/miniconda/envs/sispo/lib/python3.7/site-packages"
+export BUILD_CMAKE_ARGS
+make bpy
+#fi
