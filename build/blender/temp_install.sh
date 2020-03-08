f73b0a687f2ff7092326d87f2e3b83cbf5d9d8a3
YgabrielsY <33376611+YgabrielsY@users.noreply.github.com>
2020-03-08 19:09:47 +0200

    Try using cache

--- build/blender/temp_install.sh
+++ build/blender/temp_install.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
+if -d /home/travis/build/YgabrielsY/sispo/software/blender/build_blender_bpy/bin; then
+    echo Using cached
+    cd software/blender/build_blender_bpy
+    make install
+else
+    echo "Installing blender as a python module start"
 
-echo "Installing blender as a python module start"
+    # Creating blender directory
+    cd software
+    [[ -d blender ]] || mkdir blender  
+    cd blender
 
-# Creating blender directory
-cd software
-[[ -d blender ]] || mkdir blender
-cd blender
+    # Get blender repo
+    git clone https://git.blender.org/blender.git
+    cd blender
+    #git submodule update --init --recursive
+    #git submodule foreach git checkout master
+    #git submodule foreach pull --rebase origin master
 
-# Get blender repo
-git clone https://git.blender.org/blender.git
-cd blender
-#git submodule update --init --recursive
-#git submodule foreach git checkout master
-#git submodule foreach pull --rebase origin master
+    # Dependencies
+    chmod +x build_files/build_environment/install_deps.sh
+    ./build_files/build_environment/install_deps.sh --with-all --no-confirm --skip-oidn --skip-osd
 
-# Dependencies
-chmod +x build_files/build_environment/install_deps.sh
-./build_files/build_environment/install_deps.sh --with-all --no-confirm --skip-oidn --skip-osd
+    # Update files
+    make update
 
-# Update files
-make update
-
-# Bpy
-BUILD_CMAKE_ARGS="-DPYTHON_SITE_PACKAGES:FILEPATH=/home/travis/miniconda/envs/sispo/lib/python3.7/site-packages"
-export BUILD_CMAKE_ARGS
-make bpy
+    # Bpy
+    BUILD_CMAKE_ARGS="-DPYTHON_SITE_PACKAGES:FILEPATH=/home/travis/miniconda/envs/sispo/lib/python3.7/site-packages"
+    export BUILD_CMAKE_ARGS
+    make bpy
+fi
