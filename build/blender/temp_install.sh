58fc9912739d30bba985c3ee30a310c7efe7e204
YgabrielsY <33376611+YgabrielsY@users.noreply.github.com>
2020-03-08 20:36:42 +0200

    Remove useless stuff in temp_install add python basic tests again

--- build/blender/temp_install.sh
+++ build/blender/temp_install.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-#if [[ -f /home/travis/build/YgabrielsY/sispo/software/blender/build_linux_bpy/bin/bpy.so ]]; then
-#    echo "Using cached bpy"
-#    cd software/blender/build_linux_bpy
-#    make install
-#else
 echo "Installing blender as a python module start"
 
 # Creating blender directory
@@ -18,9 +13,6 @@ else
     git clone https://git.blender.org/blender.git
 fi
 cd blender
-#git submodule update --init --recursive
-#git submodule foreach git checkout master
-#git submodule foreach pull --rebase origin master
 
 # Dependencies
 chmod +x build_files/build_environment/install_deps.sh
@@ -33,4 +25,3 @@ make update
 BUILD_CMAKE_ARGS="-DPYTHON_SITE_PACKAGES:FILEPATH=/home/travis/miniconda/envs/sispo/lib/python3.7/site-packages"
 export BUILD_CMAKE_ARGS
 make bpy
-#fi
